07000ec3efdecdfbe9413792ec20927e342f2bb6
Ascênio Sanderson <ascenio@hotmail.com>
2024-09-15 11:55:01 -0300

    refactor(nvim): remove default hover keybinding

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -15,7 +15,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
 	callback = function(event)
 		local opts = { buffer = event.buf }
 		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+		-- vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
 		vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
 		vim.keymap.set("n", "<leader>vca", vim.lsp.buf.code_action, opts)
 		vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
