21fe67894f874fe943cfabe0057c0b056043ba22
Ascênio Sanderson <ascenio@hotmail.com>
2024-09-20 00:19:57 -0300

    feat(fish): support profiles

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -8,6 +8,9 @@ end
 
 if status is-interactive
     keychain --eval --quiet -Q | source
+		if test -f ~/.config/fish/profile.fish
+			source ~/.config/fish/profile.fish
+		end
     fish_add_path ~/.platformio/penv/bin
     fish_add_path ~/.local/bin
     fish_add_path ~/dev/Android/platform-tools
