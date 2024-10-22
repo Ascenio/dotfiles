72a13bd69a0c3c506c08218978ea8015c64e5310
Ascênio Sanderson <ascenio@hotmail.com>
2024-10-22 19:26:06 -0300

    feat(nvim): remove copy-to-clipboard keybindings

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -4,6 +4,3 @@ vim.g.maplocalleader = ' '
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
 
-vim.keymap.set({'n', 'x'}, 'gy', '"+y')
-vim.keymap.set({'n', 'x'}, 'gp', '"+p')
-
