179e6a8694a6834d2bb35ca3be2c76bc8521dfa2
Ascênio Sanderson <ascenio@hotmail.com>
2024-11-01 22:42:07 -0300

    fix(nvim): set two chars for \t

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -1,6 +1,6 @@
 vim.opt.cursorline = true
 vim.opt.inccommand = 'split'
-vim.opt.listchars = { tab = '»', trail = '·', nbsp = '␣' }
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 vim.opt.shiftwidth = 2
 vim.opt.splitbelow = true
 vim.opt.splitright = true
